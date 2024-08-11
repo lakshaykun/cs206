68dd695bdda9226b0a3fd1d0a432f8ae3d96bfba
ShivamGoyal1507 <2023aib1016@iitrpr.ac.in>
2024-08-11 18:49:02 +0530

    change test file

--- test/test.cpp
+++ test/test.cpp
@@ -1,6 +1,48 @@
-#include <iostream>
+#include<iostream>
+#include<cassert>
+#include "geometry.hpp"
+#include <cmath>
 using namespace std;
 
-int main(){
-    cout << "Hello World!" << endl;
+
+void test_distsq(){
+	assert(dist({0,0},{1,1}) == (double)sqrt(2));
+	assert(dist({1,-1},{1,1}) == (double)sqrt(4));
+	assert(dist({1,1},{1,1}) == (double)sqrt(0));
+	assert(dist({-5,5},{5,-5}) == (double)sqrt(200));
+	assert(dist({0,0},{1,0}) == (double)sqrt(1));
+	std::cout<<"all test cases passed"<<std::endl;
 }
+void test_orientation(){
+	assert(orientation({0,0},{1,1},{3,2})==1);
+	assert(orientation({0,0},{1,1},{1,-1})==1);
+	assert(orientation({0,0},{0,0},{1,1})==0);
+	assert(orientation({1,-1},{1,1},{-1,1})==2);
+	assert(orientation({-1,-1},{1,1},{3,3})==0);
+}
+void test_crossproduct(){
+	assert(crossProduct({1,1},{1,1},{1,1})==0);
+	assert(crossProduct({1,1},{-1,1},{1,-1})==4);
+	assert(crossProduct({0,1},{1,100},{100,2})==9703);
+	assert(crossProduct({7,5},{5,15},{5,4})==22);
+	assert(crossProduct({1,2},{3,4},{5,6})==8);
+	assert(crossProduct({15,7},{8,7},{10,9})==-14);
+}
+void test_duplicate(){
+	assert(removeDuplicates(&{{1,1},{1,1}})=={1,1});
+
+void test_perimeter(){
+	assert(perimeter(
+	
+	
+	
+
+int main(){
+	cout<<(double)dist({0,0},{1,1})-sqrt(2)<<endl;
+	cout<<sqrt(2)<<endl;
+	test_distsq();
+	test_orientation();
+	test_crossproduct();
+	return 0;
+}
+
